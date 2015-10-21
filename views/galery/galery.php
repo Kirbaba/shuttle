d2c253f52fc2f81f5673e42da7c24c3eca5eb95e
Korol Dima <korol_dima@list.ru>
2015-10-21 14:02:58 +0300

    shuttle

--- views/galery/galery.php
+++ views/galery/galery.php
@@ -1,7 +1,7 @@
 
 <div>
     <script type="text/javascript">
-        hs.graphicsDir = 'highslide/graphics/';
+        hs.graphicsDir = '<?=get_template_directory_uri();?>/highslide/graphics/';
         hs.align = 'center';
         hs.transitions = ['expand', 'crossfade'];
         hs.wrapperClassName = 'dark borderless floating-caption';
@@ -26,9 +26,10 @@
 <?php
 $link = $result['link'][0]->path;
 $i = 0;
+
 foreach($result['img'] as $img){
     ?>
-    <a class="highslide" onclick="return hs.expand(this)" href="<?=$link?>/<?=$result['img'][$i]->filename?>"> <img src="<?=$link?>/<?=$result['img'][$i]->filename?>" alt="" width="100px"/></a>
+    <a class="highslide" onclick="return hs.expand(this)" href="<?=$link?>/<?=$result['img'][$i]->filename?>"> <img src="<?=$link?>/thumbs/thumbs_<?=$result['img'][$i]->filename?>" alt="" width="166px"/></a>
 <?php
     $i++;
 }
--- views/slider_img_events.php
+++ views/slider_img_events.php
@@ -13,11 +13,11 @@
                         <?php
                         if ( is_user_logged_in() ) {
                             ?>
-                            <a href="<?= $result['link']; ?>/img_events/<?= $img->images; ?>" download >Скачать в HD</a>
+                            <a class="download_img" href="<?= $result['link']; ?>/img_events/<?= $img->images; ?>" download >Скачать в HD</a>
                         <?php }
                         ?>
-                        <a href="http://www.facebook.com/sharer.php?u=<?= $result['link']; ?>/img_events/<?= $img->images; ?>" target="_blank"><i class="fa fa-facebook"></i>Поделиться фотографией</a>
-                        <a href="http://vk.com/share.php?url=<?= $result['link']; ?>/img_events/<?= $img->images; ?>" target="_blank"><i class="fa fa-vk"></i>Поделиться фотографией</a>
+                        <a class="fb" href="http://www.facebook.com/sharer.php?u=<?= $result['link']; ?>/img_events/<?= $img->images; ?>" target="_blank">Поделиться фотографией</a>
+                        <a class="vk" href="http://vk.com/share.php?url=<?= $result['link']; ?>/img_events/<?= $img->images; ?>" target="_blank">Поделиться фотографией</a>
                     </div>
                 </div>
             </div>
